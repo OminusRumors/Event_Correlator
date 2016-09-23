36e472a53febe1c15571e94f597d069b49ade604
OminusRumors <georgevassiliadis@hotmail.com>
2016-09-23 11:39:18 +0200

    added security log parser

---
+++ simple-security-parser.py
@@ -0,0 +1,52 @@
+import xml.etree.ElementTree as ET
+import sqlite3 as sql
+import datetime
+from timeit import default_timer
+
+tree = ET.parse('C:/Users/George/Desktop/Logs/ad.xml')
+root = tree.getroot()
+conn = sql.connect('test.db')
+cur = conn.cursor()
+start_time = default_timer()
+
+def concat(str_value):
+	if str_value.startswith('{}'):
+		return str_value[2:]
+	elif str_value.startswith("{'SystemTime': "):
+		print (str_value[16:46])
+		return str_value[16:46]
+	else:
+		return str_value
+
+#{'SystemTime': '2016-09-16T08:34:45.700979700Z'}
+for child in root[:10]: #tag="{http://schemas.microsoft.com/win/2004/08/events/event}Provider"
+	values = []
+	data_str = ''
+	#Get the "data" part of the log file.
+	for c in child[1]:
+		data_str += concat(str(c.attrib)) + concat(str(c.text))
+	#Get the "system" part of the log file.
+	for c in child[0]:
+		print (c.tag)
+		if c.text is None and c.attrib is not None: 
+			if c.tag == '{http://schemas.microsoft.com/win/2004/08/events/event}TimeCreated':
+				dt = datetime.strptime(concat(c.attrib), '%Y-%m-%dT%H:%M:%S.%f')
+				dt_date = dt.date()
+				dt_time = dt.time()
+				values.append(dt_date, dt_time)	
+			else:
+				values.append(concat(str(c.attrib)))
+		elif c.attrib is None and c.text is not None:
+			values.append(concat(c.text))
+		elif c.attrib is not None and c.text is not None:
+			values.append(concat(str(c.attrib) + concat(c.text)))
+		else:
+			values.append(concat(c.text))
+	for v in values:
+		print(v)
+	values.append(concat(data_str))
+	cur.execute('INSERT INTO [Security-parsed-log] VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', values)
+	conn.commit()
+		#print (child[1][0].tag, child[1][0].attrib, child[1][0].text)
+		#cur.execute('INSERT INTO Security-parsed-log VALUES ')
+print ("Duration: " + str(default_timer()-start_time))
