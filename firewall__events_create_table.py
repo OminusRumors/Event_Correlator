7018a722b1078dbec440310767758312d00ebd34
OminusRumors <georgevassiliadis@hotmail.com>
2016-10-06 17:21:51 +0200

    create table for firewall events and successfully parse the data NEXT: parse the data and write parser for firewall traffic

--- firewall__events_create_table.py
+++ firewall__events_create_table.py
@@ -2,14 +2,14 @@ import re
 import pdb
 import sqlite3 as sql
 
-usr=input("Type 1 for test, 2 for events log, 3 for traffic log: ")
+usr=input("Type 1 for test, 2 for events log: ")
 
 if usr == str(1):
 	file=open("C:/Users/George/Desktop/Logs/forti_mini.csv", "r")
 elif usr == str(2):
 	file=open("C:/Users/George/Desktop/Logs/forti_event_9_27.csv", "r")
-elif usr == 3:
-	file=open("C:/Users/George/Desktop/Logs/forti_traffic_12_15.csv", "r")
+#elif usr == str(3):
+#	file=open("C:/Users/George/Desktop/Logs/forti_traffic_12_15.csv", "r")
 
 attr_list=[]
 attr_str=""
@@ -17,7 +17,7 @@ counter = 0
 saver = open("C:/Users/George/Desktop/Logs/list_bef.txt", "w")
 saver2 = open("C:/Users/George/Desktop/Logs/attr.txt", "w")
 saver3 = open("C:/Users/George/Desktop/Logs/list_aft.txt", "w")
-file=file.readlines()[:40000]
+file=file.readlines()
 
 for line in file:
 	line_list=[]
@@ -52,9 +52,10 @@ for line in file:
 		if attr not in attr_list and not attr.startswith(' '):
 			attr_list.append(attr)
 			saver2.write(attr + "\n")
-	saver3.write(str(counter) + "-" * 30)
+	#saver3.write(str(counter) + "-" * 30)
 	counter += 1
 
+'''
 for i in attr_list[:-1]:
 	attr_str += '{} VARCHAR,'.format(i)
 attr_str += '{} VARCHAR'.format(line_list[-1])
@@ -65,7 +66,7 @@ conn=sql.connect('C:/Users/George/Desktop/software tools/test.db')
 cur=conn.cursor()
 cur.execute(query)
 conn.commit()
-conn.close()
+conn.close()'''
 saver.close()
 saver2.close()
 saver3.close()
---
+++ firewall_event_parser.py
@@ -0,0 +1,33 @@
+import sqlite3 as sql
+import re
+
+file = open("C:/Users/George/Desktop/Logs/forti_event_9_27.csv", "r")
+
+con = sql.connect('C:/Users/George/Desktop/software tools/test.db')
+cur = con.cursor()
+
+for line in file:
+	attr_str=""
+	inst_list=[]
+	line_list=[]
+	line_list=re.split('[^\w+=?\w*|\-|\.|\:|\/|\s]', line)
+	line_list=list(filter(None, line_list))
+
+	#Assign alone keys with their values
+	for i in range(0, len(line_list)):
+		if line_list[i].endswith("="):
+			line_list[i] += line_list[i+1]
+
+	#Delete lone values after assignment and populate attr and inst lists
+	for i in line_list:
+		if '=' not in i:
+			line_list.pop(line_list.index(i))
+			continue
+		else:
+			attr_str += "{} ,".format(i.split('=')[0])
+			inst_list.append(i.split('=')[1])
+	attr_str = attr_str[:-1]
+	query = "INSERT OR REPLACE INTO firewall_event_log (%s) VALUES (%s)" % (attr_str, ", ".join("?" * len(inst_list)))
+	cur.execute(query, inst_list)
+	con.commit()
+con.close()
