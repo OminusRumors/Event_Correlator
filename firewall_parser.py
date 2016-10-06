a0d90c8a9d9e428a963b345b8a07654676575879
OminusRumors <georgevassiliadis@hotmail.com>
2016-10-06 11:36:31 +0200

    firewall parser partially working

--- firewall_parser.py
+++ firewall_parser.py
@@ -1,16 +1,18 @@
 import re
-import pdb
 
-usr=input("Type 1 for test, 2 for log file: ")
+usr=input("Type 1 for test, 2 for events log, 3 for traffic log: ")
 
 if usr == str(1):
 	file=open("C:/Users/George/Desktop/Logs/forti_mini.csv", "r")
 elif usr == str(2):
 	file=open("C:/Users/George/Desktop/Logs/forti_event_9_27.csv", "r")
-else:
-	usr=input("Type 1 for test, 2 for log file: ")
+elif usr == 3:
+	file=open("C:/Users/George/Desktop/Logs/forti_traffic_12_15.csv", "r")
 
 attr_list=[]
+saver = open("C:/Users/George/Desktop/Logs/list.txt", "w")
+saver2 = open("C:/Users/George/Desktop/Logs/list2.txt", "w")
+file=file.readlines()[:10000]
 
 for line in file:
 	line_list=[]
@@ -18,33 +20,18 @@ for line in file:
 	line_list=re.split('[^\w+=?\w*|\-|\.|\:|\/]', line)
 	line_list=list(filter(None, line_list))
 	for i in line_list:
-		if i.endswith('=') and line_list.index(i) < len(line_list)-1:
-			line_list[line_list.index(i)] += line_list[line_list.index(i)+1]
-			try:
-				del_list.append(line_list.index(i)+1)
-			except ValueError:
-				del_list.append(line_list.index(i))
-				print ("The error...")
-				pass
-			''''
-			try:
-				line_list.pop(line_list.index(i)+1)
-			except ValueError:
-				print ("The error...")
-				pass'''
-	for ind in del_list:
-		line_list.pop(ind)
-	print (line_list)
-'''
-	for i in range(len(line_list)-1):
-		pdb.set_trace()
-		if line_list[i].endswith('=') and i <= len(line_list)-2:
-			line_list[i] += line_list[i+1]
-			del line_list[i+1]
-			#line_list.insert(i, line_list[i]+line_list.pop(i+1))
-			print (line_list[i])
-			if attr not in attr_list:
-				attr_list.append(attr) #returns 2 value list [attribute, instance]
-'''
-for item in attr_list:
-	print(item)
+		saver.write("{}: {}\n".format(line_list.index(i), i))
+	for i in line_list:
+		if "=" not in i:
+			svd_idx = line_list.index(i)
+			line_list[svd_idx - 1] += line_list[svd_idx]
+			del line_list[svd_idx]
+	for i in line_list:
+		attr = i.split('=')[0]
+		if attr not in attr_list:
+			attr_list.append(attr)
+
+for i in attr_list:
+	saver2.write("{}: {}\n".format(attr_list.index(i) + 1, i))
+saver.close()
+saver2.close()
