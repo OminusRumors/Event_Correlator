309d68df1587c65d25c79d8c0f12385b4a2c75f5
OminusRumors <georgevassiliadis@hotmail.com>
2016-10-07 17:10:00 +0200

    firewall traffic parser working

--- firewall_traffic_get_cols.py
+++ firewall_traffic_get_cols.py
@@ -1,22 +1,26 @@
 import csv
+import sqlite3 as sql
 
-attr_list=[]
-inst_list=[]
-attr_str
 saver = open('C:/Users/George/Desktop/Logs/attr2.txt', 'w')
 with open('C:/Users/George/Desktop/Logs/forti_traffic_12_15.csv', newline='') as file:
+	con = sql.connect('C:/Users/George/Desktop/software tools/test.db')
+	cur = con.cursor()
 	reader=csv.reader(file)
-	for i in reader[:10000]:
+	for i in range(10000):
 		row = next(reader)
+		attr_str = ""
+		inst_list=[]
 		for item in row:
 			if item == '':
 				del item
 			else:
 				try:
 					attr = item.split('=')[0]
-					inst = item.split('=')[1]
-					if attr not in attr_list:
-						attr_list.append(attr)
-						saver.write(attr + "\n")
+					inst_list.append(item.split('=')[1])
+					attr_str += attr + ", "
 				except:
 					print ("You fucked up: {}".format(item))
+		attr_str = attr_str[:-2]
+		query = "INSERT OR REPLACE INTO firewall_traffic_log (%s) VALUES (%s)" % (attr_str, ", ".join("?" * len(inst_list)))
+		cur.execute(query, inst_list)
+		con.commit()
