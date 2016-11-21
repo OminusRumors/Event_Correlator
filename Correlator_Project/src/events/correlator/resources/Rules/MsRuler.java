dececa50a8730c59c582e6e8825c21ba9a871a43
OminusRumors <georgevassiliadis@hotmail.com>
2016-11-21 16:58:24 +0100

    sliding window implemented for eventid 4625

--- Correlator_Project/src/events/correlator/resources/Rules/MsRuler.java
+++ Correlator_Project/src/events/correlator/resources/Rules/MsRuler.java
@@ -1,5 +1,6 @@
 package events.correlator.resources.Rules;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -20,18 +21,35 @@ public class MsRuler {
 	
 	public void checkId4625(){
 		List<MsEvent> eventList=dbc.getMsFilteredByEventId(4625);
-		for (int i =0; i<eventList.size(); i++){
-			Date timeCreated=eventList.get(i).getCreated();
+		List<MsEvent> newList=new ArrayList<MsEvent>();
+		
+		for (MsEvent e : eventList){
+			Date timeCreated=e.getCreated();
 			Calendar curDate =Calendar.getInstance();
 			curDate.setTime(timeCreated);
+			
 			Calendar minDate = Calendar.getInstance();
 			minDate.setTime(timeCreated);
-			minDate.add(Calendar.SECOND, -3);
+			minDate.add(Calendar.SECOND, -2);
 			
 			Calendar maxDate=Calendar.getInstance();
 			maxDate.setTime(timeCreated);
-			maxDate.add(Calendar.SECOND, +3);
+			maxDate.add(Calendar.SECOND, +2);
+
+			innerloop:
+			for (int i =eventList.indexOf(e); i<eventList.size(); i++){
+				if (eventList.get(i).getCreated().compareTo(minDate.getTime())>0 && 
+						eventList.get(i).getCreated().compareTo(maxDate.getTime())<0){
+					newList.add(eventList.get(i));
+				}
+				if (eventList.get(i).getCreated().compareTo(maxDate.getTime())>0){
+					break innerloop;
+				}
+			}
 			
+			if (newList.size() >= 5){
+				// TODO: implement Alert and Scan
+			}
 		}
 	}
 	
